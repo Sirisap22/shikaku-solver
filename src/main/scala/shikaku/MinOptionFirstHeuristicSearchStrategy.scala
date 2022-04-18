bd97dcb0ad0d3671b4ead807b20d18eca6c2a6e3
Sirisap22 <boom.sirisap@gmail.com>
2022-04-18 09:31:06 +0700

    some comments'

--- src/main/scala/shikaku/MinOptionFirstHeuristicSearchStrategy.scala
+++ src/main/scala/shikaku/MinOptionFirstHeuristicSearchStrategy.scala
@@ -3,8 +3,12 @@ package shikaku
 package shikaku
 
 import scala.collection.mutable.Stack
+import scala.collection.mutable.HashMap
 
 class MinOptionFirstHeuristicSearchStrategy extends SolveStrategy {
+
+    // caching
+    //val memoize: HashMap[String, Vector[Square]] = new HashMap[String, Vector[Square]]();
     override def solve(numberOfRows: Int, numberOfCols: Int, _clues: Vector[Clue]): Vector[Square] = {      
       val stack = new Stack[(Space, Vector[Clue], Vector[Square])]()
 
@@ -37,70 +41,76 @@ class MinOptionFirstHeuristicSearchStrategy extends SolveStrategy {
   }
 
   def heuristic(clues: Vector[Clue], currentClue: Clue, currentState: Array[Array[Int]]): Vector[Square] = {
+    //val key = currentClue.toString+currentState.toString
+    //if (this.memoize.contains(key))
+    //  return this.memoize(key)
     // calcute number of possible squares that can be placed
     val shapes = this.squareShapeCombinations(currentClue.size)
     val possibleSquares = this.possibleSquareCombinations(currentState, currentClue.position, shapes, clues)
+    //this.memoize += key -> possibleSquares
     return possibleSquares
   }
 }
 
 object MinOptionFirstHeuristicSearchStrategy {
   def main(args: Array[String]) {
-    val input = Vector((0, 0, 4),
-    (6, 0, 9),
-    (8, 0, 6),
-    (7, 1, 6),
-    (9, 1, 9),
-    (16, 0, 4),
-    (17, 1, 4),
-    (14, 1, 6),
-    (15, 2, 2),
-    (12, 2, 4),
-    (13, 3, 8),
-    (6, 3, 4),
+    val input = Vector((1, 0, 5),
+    (1, 1, 3),
+    (3, 1, 2),
     (3, 2, 4),
-    (1, 2, 6),
-    (2, 3, 4),
-    (1, 4, 3),
-    (7, 4, 8),
-    (9, 4, 4),
-    (8, 5, 6),
-    (10, 5, 4),
-    (11, 6, 6),
-    (16, 5, 3),
-    (15, 6, 4),
-    (16, 7, 6),
+    (5, 2, 9),
+    (5, 3, 4),
+    (7, 3, 2),
+    (7, 4, 6),
+    (1, 5, 3),
+    (2, 5, 8),
+    (1, 6, 4),
+    (2, 6, 4),
+    (0, 9, 8),
+    (4, 7, 4),
+    (4, 8, 4),
+    (5, 7, 2),
+    (5, 8, 2),
+    (7, 9, 6),
+    (8, 9, 8),
+    (9, 0, 10),
+    (10, 0, 4),
+    (10, 5, 3),
+    (10, 6, 4),
+    (12, 6, 4),
+    (12, 7, 6),
     (14, 7, 6),
-    (17, 9, 6),
-    (10, 8, 4),
-    (11, 9, 2),
-    (8, 8, 4),
-    (9, 9, 6),
-    (0, 8, 6),
-    (1, 9, 2),
-    (2, 7, 3),
-    (3, 8, 4),
-    (5, 7, 4),
-    (4, 6, 9)
+    (14, 8, 6),
+    (16, 8, 6),
+    (16, 9, 2),
+    (17, 0, 4),
+    (12, 1, 2),
+    (13, 1, 2),
+    (12, 2, 9),
+    (13, 2, 6),
+    (15, 3, 6),
+    (16, 3, 6),
+    (15, 4, 2),
+    (16, 4, 4)
 )
 
     val clues: Vector[Clue] = for (ele <- input) yield Clue(Coord(ele._1 , ele._2), ele._3)
     val sh = new MinOptionFirstHeuristicSearchStrategy ()
     
-    val time = Utils.averageRuntimeInNanoSec(1) {
+    val time = Utils.averageRuntimeInNanoSec(100) {
 
       val ans = sh.solve(10, 18, clues)
-      var state = Array.ofDim[Int](10, 18)
-      var symbol = 1
-      for (square <- ans){
-        state = sh.placeSquare(state, square, symbol)
-        symbol += 1
-      }
-      val s1 = for (row <- state) yield for (ele <- row) yield f"$ele%02d"
-      s1.map(_.mkString(" ")).zipWithIndex.foreach{
-        case (s, idx) => println(s"row $idx - $s")
-      }
-      Utils.memory()
+      // var state = Array.ofDim[Int](10, 18)
+      // var symbol = 1
+      // for (square <- ans){
+      //   state = sh.placeSquare(state, square, symbol)
+      //   symbol += 1
+      // }
+      // val s1 = for (row <- state) yield for (ele <- row) yield f"$ele%02d"
+      // s1.map(_.mkString(" ")).zipWithIndex.foreach{
+      //   case (s, idx) => println(s"row $idx - $s")
+      // }
+      // Utils.memory()
     } 
     println(s"Runtime: ${time/1000000} ms")
 
